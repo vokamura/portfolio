dd8a59a195a20f9bc9cadd487abdf5135ac82e46
vokamura <vcokamura@gmail.com>
2018-09-05 11:32:02 -0700

    Fixed header for mobile devices

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Add email address dynamically
 window.onload = function(){
     console.log("Loaded");
     const email = "vikkiokamura@gmail.com"
@@ -6,11 +7,13 @@ window.onload = function(){
 }
 
 function menuButton(){
+    //If nav bar is open, then allow body to be clicked to close
     if (!document.getElementById("navigation").classList.contains("in")){
         document.getElementsByTagName("body")[0].setAttribute("data-toggle", "collapse");
         document.getElementsByTagName("body")[0].setAttribute("data-target", ".navbar-collapse");
         document.getElementsByTagName("body")[0].classList.add("navbar-toggle");
         document.getElementsByTagName("body")[0].classList.add("navBarOpen");
+    //If nav bar is closed, remove from body ability to open navbar without clicking menu button
     } else if (document.getElementById("navigation").classList.contains("in") || document.getElementsByTagName("body")[0].hasAttribute("data-toggle")){
         document.getElementsByTagName("body")[0].removeAttribute("data-toggle", "collapse");
         document.getElementsByTagName("body")[0].removeAttribute("data-target", ".navbar-collapse");
@@ -20,6 +23,7 @@ function menuButton(){
 }
 
 function removeMenu(){
+    //If nav bar is open, and body is clicked, remove all elements so body can't be clicked to open nav
     if(document.getElementsByTagName("body")[0].hasAttribute("data-toggle") && document.getElementById("navigation").classList.contains("in")){
         document.getElementsByClassName("navbar-collapse")[0].classList.remove("in");
         document.getElementsByTagName("body")[0].removeAttribute("data-toggle", "collapse");
@@ -29,6 +33,7 @@ function removeMenu(){
     }
 }
 
+//See if screen has touch capability and if not, then have button links for apps show up automatically and not on hover
 window.onload = function (){ 
     if ("ontouchstart" in document.documentElement)
     {
